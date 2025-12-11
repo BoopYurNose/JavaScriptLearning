41211f4e1c65b94157158f5d7fee80a2e3c5f378
BoopYurNose <zacharybaca07@gmail.com>
2025-12-11 16:11:24 -0600

    Add files via upload

---
+++ WebsitePractice/Practice.js
@@ -0,0 +1,16 @@
+/*console.log("Hello");
+console.log("These are very similar to print statements")
+
+window.alert("Test");
+window.alert("This is a alert");
+
+// this is a comment
+
+ This
+is
+a
+multi-line
+comment 
+*/
+
+document.getElementById("MyH1Tag").textContent = "Testing";
