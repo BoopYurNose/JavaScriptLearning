fc3938d8628f05bf5f66a9f3faf85bf3413aef20
BoopYurNose <zacharybaca07@gmail.com>
2025-12-15 16:17:48 -0600

    Update Practice.js

--- WebsitePractice/Practice.js
+++ WebsitePractice/Practice.js
@@ -4,13 +4,64 @@ console.log("These are very similar to print statements")
 window.alert("Test");
 window.alert("This is a alert");
 
-// this is a comment
+ this is a comment
 
- This
+This
 is
 a
 multi-line
 comment 
+
+
+document.getElementById("MyH1Tag").textContent = "Hello";
+document.getElementById("MyPTag").textContent = "Testing";
+
+ Variables Number values
+
+let NumberValue = 100;
+let SmallerNumber;
+SmallerNumber = 10
+
+
+console.log(NumberValue)
+console.log(SmallerNumber)
+
+let age = 17;
+let SalePrice = 18;
+let Number = 5;
+
+console.log(typeof age)
+console.log(`You are ${age} years old`)
+console.log(`The sales price is $${SalePrice} and that's not changing`)
+console.log(`The random number is: ${Number}`)
+
+ Variables Strings
+
+let FirstName = "Zachary";
+let Thought = "Random String";
+let Code = "JavaScript";
+let Email = "ExampleEmail@gmail.com"
+
+console.log(`Your name is ${FirstName} and additionally this is a ${Thought}`)
+console.log(`You are coding in ${Code}`)
+console.log(`Your email is ${Email}`)
+
+ Bolean Values
+
+let Online = true;
+let Offline = false;
+
+let ForSale = false;
+
+
+console.log(`Are you online?: ${Online}`);
+console.log(`Are you offline?: ${Offline}`)
+
+console.log(`Is this for sale?: ${ForSale}`)
 */
 
-document.getElementById("MyH1Tag").textContent = "Testing";
+let FullName = "Zachary Baca";
+let Age = 17;
+let Student = false;
+
+document.GetElementById("p1").textContent = FullName;
